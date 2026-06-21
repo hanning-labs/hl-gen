5e2355e4435d5b6fa45766353013bcc8d45b66eb
Adam Sabra <theadamsabra@gmail.com>
2026-06-20 21:47:46 -0700

    remove social media stub

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,6 +2,5 @@
 
 from .base import CustomHook, ToolProvider
 from .currents import CurrentsTool
-from .social import SocialMediaTool
 
-__all__ = ["ToolProvider", "CustomHook", "CurrentsTool", "SocialMediaTool"]
+__all__ = ["ToolProvider", "CustomHook", "CurrentsTool"]
